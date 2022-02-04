ec8783e44ec0ab36f655e1de4fc269c9f137677c
tomineodegard-kea <tomi0105@stud.kea.dk>
2022-02-04 14:09:28 +0100

    if else statements on the operator

--- script.js
+++ script.js
@@ -5,18 +5,37 @@ const clear = document.querySelector("#clear");
 const nr1 = document.querySelector("#firstnumber");
 const nr2 = document.querySelector("#secondnumber");
 
+let readResult = document.querySelector("#results");
+const operator = document.querySelector("#operator");
+
+let result;
 let firstNumberValue = nr1.value;
-console.log(firstNumberValue);
-
 let secondNumberValue = nr2.value;
-console.log(secondNumberValue);
 
-calculate.addEventListener("click", clickCalculate);
-clear.addEventListener("click", clickClear);
+function setUp() {
+  console.log("set up");
+  // adding eventListeners to the buttons
+  calculate.addEventListener("click", clickCalculate);
+  clear.addEventListener("click", clickClear);
+}
 
 function clickCalculate() {
-  console.log("calculate");
-  // read first number here
+  console.log(firstNumberValue);
+  console.log(secondNumberValue);
+
+  if (operator.value == "add") {
+    result = firstNumberValue + secondNumberValue;
+  } else if (operator.value == "sub") {
+    result = firstNumberValue - secondNumberValue;
+  } else if (operator.value == "mul") {
+    result = firstNumberValue * secondNumberValue;
+  } else if (operator.value == "div") {
+    result = firstNumberValue / secondNumberValue;
+  }
+  console.log(result);
+  console.log(operator.value);
+
+  readResult.textContent = result;
 }
 
 function clickClear() {
