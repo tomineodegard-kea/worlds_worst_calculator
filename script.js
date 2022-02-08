7e342210e9c129f50364a7770f789c759f0da8ab
tomineodegard-kea <tomi0105@stud.kea.dk>
2022-02-08 12:13:57 +0100

    inclass demo, doing it all over for learning purposes

--- script.js
+++ script.js
@@ -1,43 +1,70 @@
 "use strict";
 
-const calculate = document.querySelector("#calculate");
-const clear = document.querySelector("#clear");
-const nr1 = document.querySelector("#firstnumber");
-const nr2 = document.querySelector("#secondnumber");
-
-let readResult = document.querySelector("#results");
-const operator = document.querySelector("#operator");
-
-let result;
-let firstNumberValue = nr1.value;
-let secondNumberValue = nr2.value;
-
+window.addEventListener("DOMContentLoaded", setUp);
 function setUp() {
-  console.log("set up");
-  // adding eventListeners to the buttons
-  calculate.addEventListener("click", clickCalculate);
-  clear.addEventListener("click", clickClear);
+  
 }
 
-function clickCalculate() {
-  console.log(firstNumberValue);
-  console.log(secondNumberValue);
 
-  if (operator.value == "add") {
-    result = firstNumberValue + secondNumberValue;
-  } else if (operator.value == "sub") {
-    result = firstNumberValue - secondNumberValue;
-  } else if (operator.value == "mul") {
-    result = firstNumberValue * secondNumberValue;
-  } else if (operator.value == "div") {
-    result = firstNumberValue / secondNumberValue;
-  }
-  console.log(result);
-  console.log(operator.value);
 
-  readResult.textContent = result;
-}
 
-function clickClear() {
-  console.log("clear result");
+
+
+
+
+
+
+
+
+// const calculate = document.querySelector("#calculate");
+// const clear = document.querySelector("#clear");
+// const nr1 = document.querySelector("#firstnumber");
+// const nr2 = document.querySelector("#secondnumber");
+
+// let resultsList = document.querySelector("#results");
+// const operator = document.querySelector("#operator");
+
+// let result;
+// let firstNumber = parseFloat(nr1.value);
+// let secondNumber = parseFloat(nr2.value);
+
+// window.addEventListener("DOMContentLoaded", setUp);
+
+// function setUp() {
+//   console.log("set up");
+//   calculate.addEventListener("click", clickCalculate);
+//   clear.addEventListener("click", clickClear);
+//   resultsList.innerHTML = "";
+// }
+
+// function clickCalculate() {
+//   console.log(firstNumber);
+//   console.log(secondNumber);
+
+  // read first number
+
+  // read second number and convert it to a number
+
+  // read operator
+
+  // calculate based on operator
+//   if (operator.value == "add") {
+//     result = firstNumber + secondNumber;
+//   } else if (operator.value == "sub") {
+//     result = firstNumber - secondNumber;
+//   } else if (operator.value == "mul") {
+//     result = firstNumber * secondNumber;
+//   } else if (operator.value == "div") {
+//     result = firstNumber / secondNumber;
+//   }
+//   console.log(result);
+//   console.log(operator.value);
+
+//   resultsList.innerHTML += result;
+// }
+
+// function clickClear() {
+//   console.log("clear result");
+  // clear the list of resluts
+  // resultsList.textContent = "";
 }
