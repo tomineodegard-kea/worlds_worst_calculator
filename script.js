0a264ce6f9d7fc2fe021a9a0ce1e1a8a37d9535f
tomineodegard-kea <tomi0105@stud.kea.dk>
2022-02-08 13:00:47 +0100

    calculator works woth comments

--- script.js
+++ script.js
@@ -1,70 +1,52 @@
 "use strict";
 
+// step 1: we need a setUp function
 window.addEventListener("DOMContentLoaded", setUp);
 function setUp() {
-  
+  // step 2: we need to be able to click the calculate button
+  document.querySelector("#calculate").addEventListener("click", calculateClick);
+  // step 3: we need to be able to click the reset/clear button
+  document.querySelector("#clear").addEventListener("click", clearClick);
 }
 
+// step 3.1: create the following functions, connected to the eventListeners
+function calculateClick() {
+  console.log("calculateClick works");
+  // step 6: get the input from the user
+  const firstNumber = Number(document.querySelector("#firstnumber").value);
+  const secondNumber = Number(document.querySelector("#secondnumber").value);
 
+  // step 4: figure out which operator the user chose
+  const operatorValue = document.querySelector("#operator").value;
+  console.log("The operator chosen is:", operatorValue);
+  let results;
 
+  let resultList = document.querySelector("#results");
+  // step 5: make the if/else statements for all the operator values
+  if (operatorValue === "add") {
+    results = firstNumber + secondNumber;
+  } else if (operatorValue === "sub") {
+    results = firstNumber - secondNumber;
+  } else if (operatorValue === "mul") {
+    results = firstNumber * secondNumber;
+  } else if (operatorValue === "div") {
+    results = firstNumber / secondNumber;
+  }
 
+  let numberOfDecimals = document.querySelector("#decimals").value;
 
+  if (document.querySelector("#doround").checked) {
+    numberOfDecimals = document.querySelector("#decimals").value;
+  }
 
+  resultList.innerHTML = `<li>${results}</li>`;
 
+  console.log("Number of decimals:", numberOfDecimals);
+  console.log("Type of:", typeof numberOfDecimals);
+  console.log("Result:", results.toFixed(numberOfDecimals));
+  console.log("Round of:");
+}
 
-
-
-
-
-// const calculate = document.querySelector("#calculate");
-// const clear = document.querySelector("#clear");
-// const nr1 = document.querySelector("#firstnumber");
-// const nr2 = document.querySelector("#secondnumber");
-
-// let resultsList = document.querySelector("#results");
-// const operator = document.querySelector("#operator");
-
-// let result;
-// let firstNumber = parseFloat(nr1.value);
-// let secondNumber = parseFloat(nr2.value);
-
-// window.addEventListener("DOMContentLoaded", setUp);
-
-// function setUp() {
-//   console.log("set up");
-//   calculate.addEventListener("click", clickCalculate);
-//   clear.addEventListener("click", clickClear);
-//   resultsList.innerHTML = "";
-// }
-
-// function clickCalculate() {
-//   console.log(firstNumber);
-//   console.log(secondNumber);
-
-  // read first number
-
-  // read second number and convert it to a number
-
-  // read operator
-
-  // calculate based on operator
-//   if (operator.value == "add") {
-//     result = firstNumber + secondNumber;
-//   } else if (operator.value == "sub") {
-//     result = firstNumber - secondNumber;
-//   } else if (operator.value == "mul") {
-//     result = firstNumber * secondNumber;
-//   } else if (operator.value == "div") {
-//     result = firstNumber / secondNumber;
-//   }
-//   console.log(result);
-//   console.log(operator.value);
-
-//   resultsList.innerHTML += result;
-// }
-
-// function clickClear() {
-//   console.log("clear result");
-  // clear the list of resluts
-  // resultsList.textContent = "";
+function clearClick() {
+  console.log("clearClick works");
 }
